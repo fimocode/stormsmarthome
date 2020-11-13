18d7607b75ac6f7cb5320e57fd5fdce435c4bb61
hiiamlala <kulz0zl0l@gmail.com>
2020-11-13 11:25:07 +0700

    Add device notification

--- src/main/java/com/storm/iotdata/StormConfig.java
+++ src/main/java/com/storm/iotdata/StormConfig.java
@@ -27,6 +27,10 @@ public class StormConfig implements Serializable{
     private boolean householdCheckMin = false;
     private boolean householdCheckAvg = true;
     private boolean householdCheckMax = true;
+    private int deviceLogGap = 20;
+    private boolean deviceCheckMin = false;
+    private boolean deviceCheckAvg = true;
+    private boolean deviceCheckMax = true;
 
     public StormConfig(){
         Yaml yaml = new Yaml();
@@ -34,9 +38,9 @@ public class StormConfig implements Serializable{
         Map<String, Object> obj = yaml.load(inputStream);
         this.cleanDatabase = (Boolean) obj.getOrDefault("database.clean", this.cleanDatabase);
         this.topologyName = (String) obj.getOrDefault("storm.topologyName", this.topologyName);
-        this.stormBrokerURL = (String) obj.getOrDefault("storm.brokerURL", this.stormBrokerURL);
-        this.stormTopicList = (List<String>) obj.getOrDefault("storm.topicList", this.stormTopicList);
-        this.stormWindowList = (List<Integer>) obj.getOrDefault("storm.windowList", this.stormWindowList);
+        this.stormBrokerURL = (String) obj.getOrDefault("spout.brokerURL", this.stormBrokerURL);
+        this.stormTopicList = (List<String>) obj.getOrDefault("spout.topicList", this.stormTopicList);
+        this.stormWindowList = (List<Integer>) obj.getOrDefault("iot analytics.windowList", this.stormWindowList);
         this.notificationMQTT = (Boolean) obj.getOrDefault("notification.publishMQTT", this.notificationMQTT);
         this.notificationBrokerURL = (String) obj.getOrDefault("notification.brokerURL", this.notificationBrokerURL);
         this.houseLogGap = (int) obj.getOrDefault("notification.house.logGap", this.houseLogGap);
@@ -47,6 +51,10 @@ public class StormConfig implements Serializable{
         this.householdCheckMin = (Boolean) obj.getOrDefault("notification.household.checkMin", this.householdCheckMin);
         this.householdCheckAvg = (Boolean) obj.getOrDefault("notification.household.checkAvg", this.householdCheckAvg);
         this.householdCheckMax = (Boolean) obj.getOrDefault("notification.household.checkMax", this.householdCheckMax);
+        this.deviceLogGap = (int) obj.getOrDefault("notification.device.logGap", this.deviceLogGap);
+        this.deviceCheckMin = (Boolean) obj.getOrDefault("notification.device.checkMin", this.deviceCheckMin);
+        this.deviceCheckAvg = (Boolean) obj.getOrDefault("notification.device.checkAvg", this.deviceCheckAvg);
+        this.deviceCheckMax = (Boolean) obj.getOrDefault("notification.device.checkMax", this.deviceCheckMax);
     }
 
     public boolean isCleanDatabase() {
@@ -199,6 +207,50 @@ public class StormConfig implements Serializable{
 
     public void setHouseholdCheckMax(boolean householdCheckMax) {
         this.householdCheckMax = householdCheckMax;
+    }
+    
+    public int getDeviceLogGap() {
+        return this.deviceLogGap;
+    }
+
+    public void setDeviceLogGap(int deviceLogGap) {
+        this.deviceLogGap = deviceLogGap;
+    }
+
+    public boolean isDeviceCheckMin() {
+        return this.deviceCheckMin;
+    }
+
+    public boolean getDeviceCheckMin() {
+        return this.deviceCheckMin;
+    }
+
+    public void setDeviceCheckMin(boolean deviceCheckMin) {
+        this.deviceCheckMin = deviceCheckMin;
+    }
+
+    public boolean isDeviceCheckAvg() {
+        return this.deviceCheckAvg;
+    }
+
+    public boolean getDeviceCheckAvg() {
+        return this.deviceCheckAvg;
+    }
+
+    public void setDeviceCheckAvg(boolean deviceCheckAvg) {
+        this.deviceCheckAvg = deviceCheckAvg;
+    }
+
+    public boolean isDeviceCheckMax() {
+        return this.deviceCheckMax;
+    }
+
+    public boolean getDeviceCheckMax() {
+        return this.deviceCheckMax;
+    }
+
+    public void setDeviceCheckMax(boolean deviceCheckMax) {
+        this.deviceCheckMax = deviceCheckMax;
     }
 
     public String toString() {
