da5e454c79327dfd5e569a3380b1c0fd2872ba49
hiiamlala <kulz0zl0l@gmail.com>
2020-12-07 11:35:24 +0700

    split purge and init forecast db

--- src/main/java/com/storm/iotdata/functions/DB_store.java
+++ src/main/java/com/storm/iotdata/functions/DB_store.java
@@ -163,6 +163,24 @@ public class DB_store {
         try {
             Connection conn = DB_store.initConnection();
             Statement stmt = conn.createStatement();
+            stmt.executeUpdate("create table if not exists house_data_forecast_" + table
+                    + " (house_id INT UNSIGNED NOT NULL, year VARCHAR(4) NOT NULL, month VARCHAR(2) NOT NULL, day VARCHAR(2) NOT NULL,slice_gap INT UNSIGNED NOT NULL, slice_index INT NOT NULL, avg DOUBLE UNSIGNED NOT NULL, reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY(house_id, year, month, day, slice_gap, slice_index))");
+            stmt.executeUpdate("create table if not exists household_data_forecast_" + table
+                    + " (house_id INT UNSIGNED NOT NULL, household_id INT UNSIGNED NOT NULL, year VARCHAR(4) NOT NULL, month VARCHAR(2) NOT NULL, day VARCHAR(2) NOT NULL,slice_gap INT UNSIGNED NOT NULL, slice_index INT NOT NULL, avg DOUBLE UNSIGNED NOT NULL, reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY(house_id, year, month, day, slice_gap, slice_index))");
+            stmt.executeUpdate("create table if not exists device_data_forecast_" + table
+                    + " (house_id INT UNSIGNED NOT NULL, household_id INT UNSIGNED NOT NULL, device_id INT UNSIGNED NOT NULL, year VARCHAR(4) NOT NULL, month VARCHAR(2) NOT NULL, day VARCHAR(2) NOT NULL,slice_gap INT UNSIGNED NOT NULL, slice_index INT NOT NULL, avg DOUBLE UNSIGNED NOT NULL, reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY(house_id, year, month, day, slice_gap, slice_index))");
+            conn.close();
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean purgeForecastTable(String table) {
+        try {
+            Connection conn = DB_store.initConnection();
+            Statement stmt = conn.createStatement();
             stmt.execute("drop table if exists house_data_forecast_" + table);
             stmt.execute("drop table if exists household_data_forecast_" + table);
             stmt.execute("drop table if exists device_data_forecast_" + table);
