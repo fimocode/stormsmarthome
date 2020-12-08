74a1f98f69b2cbe90c247b512442d66ed9f4fd52
hiiamlala <kulz0zl0l@gmail.com>
2020-12-08 12:59:47 +0700

    forecast debug

--- src/main/java/com/storm/iotdata/MainTopo.java
+++ src/main/java/com/storm/iotdata/MainTopo.java
@@ -101,8 +101,8 @@ public class MainTopo {
                             builder.setBolt("avg-" + windowSize, new Bolt_avg(windowSize, config), 1));
                     sumList.put("sum-" + windowSize,
                             builder.setBolt("sum-" + windowSize, new Bolt_sum(windowSize, config), 1));
-                    // forecastList.put("forecast-" + windowSize,
-                    //         builder.setBolt("forecast-" + windowSize, new Bolt_forecast(windowSize, config), 1));
+                    forecastList.put("forecast-" + windowSize,
+                            builder.setBolt("forecast-" + windowSize, new Bolt_forecast(windowSize, config), 1));
                 }
                 
                 for (Integer windowSize : config.getWindowList()) {
@@ -113,9 +113,9 @@ public class MainTopo {
                     avgList.get("avg-" + windowSize).shuffleGrouping("spout-trigger", "trigger");
                     sumList.get("sum-" + windowSize).shuffleGrouping("avg-" + windowSize, "data");
                     sumList.get("sum-" + windowSize).shuffleGrouping("avg-" + windowSize, "trigger");
-                    // forecastList.get("forecast-" + windowSize).shuffleGrouping("avg-" + windowSize, "data");
-                    // forecastList.get("forecast-" + windowSize).shuffleGrouping("sum-" + windowSize, "data");
-                    // forecastList.get("forecast-" + windowSize).shuffleGrouping("sum-" + windowSize, "trigger");
+                    forecastList.get("forecast-" + windowSize).shuffleGrouping("avg-" + windowSize, "data");
+                    forecastList.get("forecast-" + windowSize).shuffleGrouping("sum-" + windowSize, "data");
+                    forecastList.get("forecast-" + windowSize).shuffleGrouping("sum-" + windowSize, "trigger");
                 }
 
                 Config conf = new Config();
